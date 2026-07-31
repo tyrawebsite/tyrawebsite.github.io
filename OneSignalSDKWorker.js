ea955c908cc4f577c0591dd5407778ea3796490c
tyrawebsite <tyra@onesignal.com>
2026-07-31 15:20:10 -0700

    Add files via upload

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,9 +1,1 @@
-<script src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js" defer></script>
-<script>
-  window.OneSignalDeferred = window.OneSignalDeferred || [];
-  OneSignalDeferred.push(async function(OneSignal) {
-    await OneSignal.init({
-      appId: "5f123852-d2b0-4f07-bb23-fea5d1536cef",
-    });
-  });
-</script>
+importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
